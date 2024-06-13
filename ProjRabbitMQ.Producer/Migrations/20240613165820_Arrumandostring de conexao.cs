3e14c03d52fbeb6fabb8ce8294e026c1fce85b46
Daiane Costa <cicuto.adv@gmail.com>
2024-06-13 15:06:03 -0300

    Add project files.

---
+++ ProjRabbitMQ.Producer/Migrations/20240613165820_Arrumandostring de conexao.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjRabbitMQ.Producer.Migrations
+{
+    public partial class Arrumandostringdeconexao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ProjRabbitMQ.SendMessage/Migrations/20240613171401_veifir se tbela ja existe ante de tentar crir outra .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjRabbitMQ.SendMessage.Migrations
+{
+    public partial class veifirsetbelajaexisteantedetentarcriroutra : Migration
+    {
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Seu trecho de código personalizado para criar a tabela 'Message', se ela ainda não existir
+            migrationBuilder.Sql("IF OBJECT_ID('dbo.Message', 'U') IS NULL BEGIN CREATE TABLE dbo.Message (Id INT PRIMARY KEY) END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
